2b37cfa1532b1278e54809a528c6c83ef2afebeb
muhamaddafa <gaming102dafa@gmail.com>
2024-06-24 13:52:00 +0700

    build: dashboard galeri

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -49,6 +49,15 @@ export default function Authenticated({ user, header, children }) {
                                     Ekstrakurikuler
                                 </NavLink>
                             </div>
+
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink
+                                    href={route("dashboard_galeri")}
+                                    active={route().current("dashboard_galeri")}
+                                >
+                                    Galeri
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div className="hidden sm:flex sm:items-center sm:ms-6">
